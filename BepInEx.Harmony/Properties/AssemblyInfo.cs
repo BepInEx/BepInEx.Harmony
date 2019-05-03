e312b4d07326f72bec23d4fd61bbb83fe188bd9c
Bepis <36346617+bbepis@users.noreply.github.com>
2019-05-04 02:08:08 +1000

    Bump assembly version to 2.0

--- BepInEx.Harmony/Properties/AssemblyInfo.cs
+++ BepInEx.Harmony/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BepInEx.Harmony")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Harmony wrapper for BepInEx")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BepInEx.Harmony")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Bepis 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
