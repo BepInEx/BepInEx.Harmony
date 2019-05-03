e75d37623e1829e774bed1d78b9034e37515ed51
Bepis <36346617+bbepis@users.noreply.github.com>
2019-05-04 02:03:16 +1000

    Add test project

---
+++ BepInEx.Harmony.Tests/Assets.cs
@@ -0,0 +1,13 @@
+﻿namespace BepInEx.Harmony.Tests
+{
+	internal static class StaticAssets
+	{
+		internal static int TestStaticField = 0;
+
+		internal static void TestStaticMethod()
+		{
+			int i = int.MaxValue;
+			var b = i.CompareTo(i);
+		}
+	}
+}
---
+++ BepInEx.Harmony.Tests/HarmonyWrapperTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BepInEx.Harmony.Tests
+{
+	[TestClass]
+	public class HarmonyWrapperTests
+	{
+		[TestMethod]
+		public void EmitDelegateTest()
+		{
+			var instruction = HarmonyWrapper.EmitDelegate<Action>(StaticAssets.TestStaticMethod);
+
+			Assert.AreEqual(OpCodes.Call, instruction.opcode);
+			Assert.IsTrue(instruction.operand is MethodInfo);
+
+			instruction = HarmonyWrapper.EmitDelegate<Action>(() => StaticAssets.TestStaticField = 5);
+
+			Assert.AreEqual(OpCodes.Call, instruction.opcode);
+			Assert.IsTrue(instruction.operand is MethodInfo);
+
+			CompileInstruction(instruction)();
+
+			Assert.AreEqual(5, StaticAssets.TestStaticField);
+
+			int dummy = 0;
+
+			instruction = HarmonyWrapper.EmitDelegate<Action>(() => dummy = 15);
+
+			Assert.AreEqual(OpCodes.Call, instruction.opcode);
+			Assert.IsTrue(instruction.operand is MethodInfo);
+
+			CompileInstruction(instruction)();
+
+			Assert.AreEqual(15, dummy);
+		}
+
+		private static Action CompileInstruction(CodeInstruction instruction) =>
+			(Action)((DynamicMethod)instruction.operand).CreateDelegate(typeof(Action));
+	}
+}
