a71acb2f2efd21af88e16664b1e7f4ed9632847a
Keelhauled <keelhauledhs@gmail.com>
2019-07-03 20:38:07 +0300

    HarmonyExtensions

---
+++ BepInEx.Harmony/HarmonyExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BepInEx.Harmony
+{
+    /// <summary>
+	/// An extension class for Harmony based operations.
+	/// </summary>
+    public static class HarmonyExtensions
+    {
+        /// <summary>
+		/// Applies all patches specified in the type.
+		/// </summary>
+		/// <param name="harmonyInstance">The HarmonyInstance to use.</param>
+		/// <param name="type">The type to scan.</param>
+        public static void PatchAll(this HarmonyLib.Harmony harmonyInstance, Type type)
+        {
+            HarmonyWrapper.PatchAll(type, harmonyInstance);
+        }
+    }
+}
