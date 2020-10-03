5c7e06550ca038daba8e3fa7513d597adcfec687
ghorsington <neigh@coder.horse>
2020-10-03 17:07:51 +0300

    Add assembly resolving logic

--- HarmonyXInterop/HarmonyInterop.cs
+++ HarmonyXInterop/HarmonyInterop.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using HarmonyLib;
 using HarmonyLib.Public.Patching;
+using Mono.Cecil;
 
 namespace HarmonyXInterop
 {
@@ -14,11 +16,11 @@ namespace HarmonyXInterop
         public PatchMethod[] transpilers;
         public PatchMethod[] finalizers;
     }
-    
+
     public class PatchMethod
     {
         public string owner;
-        
+
         public MethodInfo method; // need to be called 'method'
 
         /// <summary>Priority</summary>
@@ -42,14 +44,66 @@ namespace HarmonyXInterop
             };
         }
     }
-    
+
     public static class HarmonyInterop
     {
+        private static readonly SortedDictionary<Version, string> Assemblies = new SortedDictionary<Version, string>();
+
+        public static void Initialize()
+        {
+            var curDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            foreach (var file in Directory.GetFiles(curDir, "0Harmony*.dll", SearchOption.AllDirectories))
+            {
+                using var ass = AssemblyDefinition.ReadAssembly(file);
+                Assemblies.Add(ass.Name.Version, file);
+            }
+
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveHarmonyDependency;
+        }
+
+        private static bool TryParseAssemblyName(string fullName, out AssemblyName assemblyName)
+        {
+            try
+            {
+                assemblyName = new AssemblyName(fullName);
+                return true;
+            }
+            catch (Exception e)
+            {
+                File.AppendAllText("tryparseerr.log", $"Failed to parse {fullName}: {e}");
+                assemblyName = null;
+                return false;
+            }
+        }
+
+        private static Assembly ResolveHarmonyDependency(object sender, ResolveEventArgs args)
+        {
+            if (!TryParseAssemblyName(args.Name, out var name))
+                return null;
+
+            if (!name.Name.StartsWith("0Harmony"))
+                return null;
+
+            var assToLoad = Assemblies.LastOrDefault(kv => kv.Key <= name.Version);
+            if (assToLoad.Value == null)
+                return null;
+
+            try
+            {
+                return Assembly.LoadFile(assToLoad.Value);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
         private static Action<MethodBase, PatchInfo> UpdateWrapper =
             AccessTools.MethodDelegate<Action<MethodBase, PatchInfo>>(
                 AccessTools.Method(typeof(HarmonyManipulator).Assembly.GetType("HarmonyLib.PatchFunctions"),
                     "UpdateWrapper"));
-        
+
         public static void ApplyPatch(MethodBase target, PatchInfoWrapper info)
         {
             var pInfo = target.ToPatchInfo();
@@ -60,6 +114,7 @@ namespace HarmonyXInterop
                 pInfo.transpilers = Sync(info.transpilers, pInfo.transpilers);
                 pInfo.finalizers = Sync(info.finalizers, pInfo.finalizers);
             }
+
             UpdateWrapper(target, pInfo);
         }
 
@@ -70,7 +125,8 @@ namespace HarmonyXInterop
             var toRemove = new HashSet<MethodInfo>(add.Select(a => a.method));
             current = current.Where(p => !toRemove.Contains(p.PatchMethod)).ToArray();
             var initialIndex = current.Length;
-            return current.Concat(add.Where(method => method != null).Select((method, i) => new Patch(method.ToHarmonyMethod(out var owner), i + initialIndex, owner))).ToArray();
+            return current.Concat(add.Where(method => method != null).Select((method, i) =>
+                new Patch(method.ToHarmonyMethod(out var owner), i + initialIndex, owner))).ToArray();
         }
     }
 }
